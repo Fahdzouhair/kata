913433e7ac5b3f68bf816784efcb8a17fbec17dc
fahdzouhair <fahdzouhair47@gmail.com>
2025-07-24 11:51:18 +0100

    Q2 changes

--- srv/CatalogService.cds
+++ srv/CatalogService.cds
@@ -2,7 +2,7 @@ using { my.Kata as my } from '../db/schema';
 
 Service CatalogService @(requires: 'authenticated-user') {
     @Capabilities : { 
-        Updatable:false , Deletable:false,
+       Updatable:false, Deletable:false,
      }
     entity Books as projection on my.Book ;
 
--- test/books.test.js
+++ test/books.test.js
@@ -1,69 +1,82 @@
 const cds = require('@sap/cds');
 
 describe('Books Service Tests', () => {
-  const test = cds.test(__dirname + '/..');  
+  const test = cds.test(__dirname + '/..');
   const { POST, GET, PUT, DELETE, expect } = test;
-  const newBook = {title:'kozina' , author:'saaid'};         
+  const newBook = { title: 'kozina', author: 'saaid' };
 
-  beforeEach(test.data.reset);                
-  
+  beforeEach(test.data.reset);
+
   // check authenticated user get a 401 status with POST
-  it('Books Post with Alice', async()=>{
-    const newBook = {title:'kozina' , author:'saaid'};
-    const { status , data  } = await POST('/odata/v4/catalog/Books',newBook , { auth : { username : "fahd" , password : "123456" }});
+  it('Books Post with Alice', async () => {
+    const newBooks2 = [
+      { title: 'kozina', author: 'saaid' },
+      { title: 'anotherBook', author: 'john' }
+    ];
 
-    expect(status).to.equal(201);
-    expect(data.author).to.equal(newBook.author);
+    newBooks2.map(async (book)=>{
+       const {status} = await POST('/odata/v4/catalog/Books', book, { auth: { username: "fahd", password: "123456" } })
+       expect(status).to.equal(201);
+      });
+
+      const {status , data} = await GET('/odata/v4/catalog/Books', { auth: { username: 'fahd', password: '123456' } });
+      console.log('all books' , data.value);
+    //const { status, data } = await POST('/odata/v4/catalog/Books', newBook, { auth: { username: "fahd", password: "123456" } });
+    //console.log('response' , {status , data});
+    //expect(status).to.equal(201);
+    //expect(data.author).to.equal(newBook.author);
   })
 
   // check unauthenticated user get a 401 status with POST
-  it('Books POST with unauthenticated user -> 401', async()=>{
-    
-    try{
-        const { status , data  } = await POST('/odata/v4/catalog/Books',newBook);
-   
-    }catch(err){
-         expect(err.response.status).to.equal(401);
+  it('Books POST with unauthenticated user -> 401', async () => {
+
+    try {
+      
+      await POST('/odata/v4/catalog/Books', newBook);
+
+    } catch (err) {
+      expect(err.status).to.equal(401);
     }
   })
 
   // check authenticated user get a 401 status with GET
-  it('Books Get With Alice User', async()=>{
-    const {status, data} = await GET('/odata/v4/catalog/Books', { auth: { username : 'fahd' , password : '123456' }});
+  it('Books Get With Alice User', async () => {
+    const { status } = await GET('/odata/v4/catalog/Books', { auth: { username: 'fahd', password: '123456' } });
+    //console.log("Response data from GET request:", data.value);
     expect(status).to.eql(200);
   })
 
 
   // check unauthenticated user get a 401 status with GET
-  it('Books Get With  unauthenticated user -> 401', async()=>{
-    try{
-      const {status, data} = await GET('/odata/v4/catalog/Books');
+  it('Books Get With  unauthenticated user -> 401', async () => {
+    try {
 
-    }catch(err){
-      expect(err.response.status).to.eql(401);
+      await GET('/odata/v4/catalog/Books');
+      
+    } catch (err) {
+      expect(err.status).to.eql(401);
     }
-  })  
+  })
 
   // check PUT Books is rejected with 405
-  it('Book PUT Test ->405',async()=>{
-    try{
-      const { status } = await PUT('/odata/v4/catalog/Books',newBook, { auth : {username : "fahd" , password : "123456"}});
-
-    }catch(err){
-      console.log(err);
-      expect(err.response.status).to.eql(405);
+  it('Book PUT Test ->405', async () => {
+    try {
+      await PUT('/odata/v4/catalog/Books(6b4e801c-c081-440d-8353-457767a327a4)', { title: "ana", author: '8' }, { auth: { username: "fahd", password: "123456" } });
+      expect.fail();
+    } catch (error) {
+      expect(error?.status).to.eql(405, error.message);
     }
   })
 
-  it('Book DELETE Test ->405',async()=>{
-    try{
-      const { status } = await DELETE('/odata/v4/catalog/Books', { auth : {username : "fahd" , password : "123456"}});
 
-    }catch(err){
-      console.log(err);
-      expect(err.response.status).to.eql(405);
+  // check Delete Books is rejected with 405
+  it('Book DELETE Test ->405', async () => {
+    try {
+      await DELETE('/odata/v4/catalog/Books(7812e4c4-9db5-4176-bb64-1b216bb2f742)', { auth: { username: "fahd", password: "123456" } });
+      expect.fail('method not allowed');
+    } catch (error) {
+      console.log(error);
+      expect(error?.status).to.eql(405, error.message);
     }
   })
-  
-
 });
